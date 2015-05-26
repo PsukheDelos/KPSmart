42dd70c1dba91a622ad5d7b500e102d8ea8ae7de
Glen Peek <glen.peek@gmail.com>
2015-05-26 23:01:00 +1200

    removed getPricesTable()

--- src/kps/backend/database/PriceRepository.java
+++ src/kps/backend/database/PriceRepository.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
@@ -24,39 +23,13 @@ public class PriceRepository {
 		}
 		return true;
 	}
-
-	public static JTable getPricesTable(){
-		if(!thereIsAConnectionToTheDatabase()) db = KPSDatabase.createConnection();
-		try {
-			DefaultTableModel model = new DefaultTableModel() {
-				@Override
-				public boolean isCellEditable(int row, int column) {
-					//all cells false
-					return false;
-				}
-			};
-			JTable prices = new JTable(model);
-			model.addColumn("from");
-			model.addColumn("to");
-			model.addColumn("priority");
-			model.addColumn("price per gram");
-			model.addColumn("price per volume");
-			Statement statement = db.createStatement();
-			String query = "SELECT * FROM price";
-			ResultSet result = statement.executeQuery(query);
-			while(result.next()){
-				model.addRow(new Object[] {result.getString(1), result.getString(2), result.getString(3), result.getDouble(4), result.getDouble(5)});
-			}
-			db.close();
-			return prices;
-		} catch (SQLException e) {e.printStackTrace();}
-		return null;
-	}
 	
 	public static TableModel getPricesModel(){
 		if(!thereIsAConnectionToTheDatabase()) db = KPSDatabase.createConnection();
 		try {
 			DefaultTableModel model = new DefaultTableModel() {
+				private static final long serialVersionUID = 1L;
+
 				@Override
 				public boolean isCellEditable(int row, int column) {
 					//all cells false
