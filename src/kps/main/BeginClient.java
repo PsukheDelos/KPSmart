c8d2f6a89da3fd911c553510ec6897c2a6daba92
Glen Peek <glen.peek@gmail.com>
2015-06-04 21:40:18 +1200

    cleaned up

--- src/kps/main/BeginClient.java
+++ src/kps/main/BeginClient.java
@@ -1,12 +1,10 @@
 package kps.main;
-import kps.frontend.gui.ClientFrame;
 
-import com.alee.laf.WebLookAndFeel;
+import kps.frontend.gui.ClientFrame;
 
 public class BeginClient {
 
 	public static void main(String[] args){
-		//WebLookAndFeel.install();
 		new ClientFrame();
 	}
 
