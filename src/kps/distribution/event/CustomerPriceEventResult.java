b4aae6270ac55c977cc1e7a38e8331d4256c9c41
Glen Peek <glen.peek@gmail.com>
2015-06-06 17:07:41 +1200

    giving all event results an ID

--- src/kps/distribution/event/CustomerPriceEventResult.java
+++ src/kps/distribution/event/CustomerPriceEventResult.java
@@ -1,9 +1,5 @@
 package kps.distribution.event;
 
-import java.util.UUID;
-
 public class CustomerPriceEventResult extends EventResult {
 	private static final long serialVersionUID = 1L;
-
-	public UUID id;
 }
--- src/kps/distribution/event/EventResult.java
+++ src/kps/distribution/event/EventResult.java
@@ -1,8 +1,10 @@
 package kps.distribution.event;
 
+import java.util.UUID;
+
 import kps.net.event.Event;
 
 public abstract class EventResult extends Event{
 	private static final long serialVersionUID = 1L;
-
+	public UUID id;
 }
