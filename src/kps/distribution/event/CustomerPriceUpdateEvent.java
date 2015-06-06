88eeb1604441f4fcbb4ee1e44f7a5dc219a1a4be
Glen Peek <glen.peek@gmail.com>
2015-06-06 14:57:31 +1200

    reimplementing customerpriceupdateevent to extend customerPriceEvent

---
+++ src/kps/distribution/event/CustomerPriceUpdateEvent.java
@@ -0,0 +1,12 @@
+package kps.distribution.event;
+
+public class CustomerPriceUpdateEvent extends CustomerPriceEvent {
+
+	private static final long serialVersionUID = 1L;
+	
+	public CustomerPriceUpdateEvent(String from, String to, String priority,
+			double weightCost, double volumeCost) {
+		super(from, to, priority, weightCost, volumeCost);
+	}
+
+}
