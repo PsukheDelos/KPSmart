9c8accc106e6f657f46f8682dd09054cc39203a2
Glen Peek <glen.peek@gmail.com>
2015-06-07 22:19:45 +1200

    making UpdateTableEvent Global

--- src/kps/distribution/event/UpdateTableEvent.java
+++ src/kps/distribution/event/UpdateTableEvent.java
@@ -3,7 +3,9 @@ package kps.distribution.event;
 import javax.swing.JTable;
 import javax.swing.table.TableModel;
 
-public abstract class UpdateTableEvent extends DistributionNetworkEvent{
+import kps.interfaces.IGlobalEvent;
+
+public abstract class UpdateTableEvent extends DistributionNetworkEvent implements IGlobalEvent{
 	private static final long serialVersionUID = 1L;
 	
 	public UpdateTableEvent(JTable table, TableModel model){
